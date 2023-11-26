c806314b255b8b48c853f0032099a0d4c69d26d6
AhnyO1966 <akpanenyinv@gmail.com>
2023-11-26 15:15:13 +0100

    Installed and configured our database using configModule package

---
+++ src/database/database.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+    imports:[
+        TypeOrmModule.forRootAsync({      
+            useFactory:(configService:ConfigService)=>({
+              type: 'mysql',
+              host: configService.getOrThrow('DB_HOST'),
+              port: configService.getOrThrow('DB_PORT'),
+              username: configService.getOrThrow('DB_USER'),
+              password: configService.getOrThrow('DB_PASSWORD'),
+              database: configService.getOrThrow('DB_NAME'),
+              synchronize: configService.getOrThrow('DB_SYNCHRONIZE'),
+              autoLoadEntities: true,
+      
+            }),
+        
+               inject:[ConfigService],
+              }),
+    ]
+})
+export class DatabaseModule {}
